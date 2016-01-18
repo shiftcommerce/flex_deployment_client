40cd54865e648e3f11d3643db914e0c762d9d10e
Gary Taylor <gary.taylor@flexcommerce.com>
2016-01-18 16:16:07 +0000

    Added option for the client to provide a file URL to send the data as base64 encoded to the server

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -3,6 +3,15 @@ require "flex_deployment_client/requestor/uploaded_file_requestor"
 module FlexDeploymentClient
   class UploadedFile < ModelBase
 
+    def file=(value)
+      if value=~/^file:\/\//
+        filename = value.match(/^file:\/\/(.*)$/)[1]
+        MIME::Types.type_for(filename).first.content_type
+        bin_data = File.read_binary(filename)
+        super("data:image/jpg;base64,#{Base64.encode64(bin_data)}")
+      end
+    end
+
     def self.upload(attributes)
       parser.parse(self, connection.run(:post, table_name, { data: { type: :uploaded_files, attributes: attributes } }, custom_headers.merge({ "Content-Type": "multipart/form-data" }))).first
     end
--- lib/flex_deployment_client/version.rb
+++ lib/flex_deployment_client/version.rb
@@ -1,3 +1,3 @@
 module FlexDeploymentClient
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
