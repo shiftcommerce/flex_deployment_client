d1e861ac87c7f1c632ad26df28216623a4565073
Paul Springett <paul@springett.me>
2021-09-27 11:31:23 +0100

    Replace custom class with config options

--- app/models/model_base.rb
+++ app/models/model_base.rb
@@ -1,31 +1,22 @@
 require "flex_deployment_client/connection/flexible_connection"
+
 module FlexDeploymentClient
   class ModelBase < JsonApiClient::Resource
-    self.site = FlexDeploymentClient.config.root_url
-    self.connection_class = FlexibleConnection
+    config = FlexDeploymentClient.config
 
-    connection_options[:adapter] = FlexDeploymentClient.config.adapter || :net_http
+    self.site = config.root_url
 
-    class << self
-      def reconfigure_all
-        subclasses.each do |sub|
-          sub.reconfigure
-        end
-        reconfigure
-      end
+    self.connection_options = {
+      adapter: config.adapter
+    }
 
-      def reconfigure
-        self.site = FlexDeploymentClient.config.root_url
-        self.connection_class = FlexibleConnection
-        self.connection_options = connection_options.merge adapter: FlexDeploymentClient.config.adapter || :net_http
-        reload_connection_if_required
-      end
-
-      def reload_connection_if_required
-        _build_connection(true) if connection_object
-      end
+    connection do |conn|
+      conn.use Faraday::Request::Authorization, :basic, config.user, config.password
+      conn.use Faraday::Request::Json
+      conn.use Faraday::Request::Multipart
+      conn.use FlexibleJsonRequest
+      conn.use JsonApiClient::Middleware::Status
+      conn.use JsonApiClient::Middleware::ParseJson
     end
-
-    reconfigure
   end
 end
