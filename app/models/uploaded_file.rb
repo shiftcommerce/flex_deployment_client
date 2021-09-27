4b5c4c1fc65e3b15aee655e78c326e0e567429ec
Paul Springett <paul@springett.me>
2021-09-27 15:38:00 +0100

    Remove broken method
    UploadedFile#file= only ever received a hash containing a `url`
    property, therefore the regex conditionals never worked and only
    super was ever called.

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -2,25 +2,12 @@ require "flex_deployment_client/middleware/uploaded_file_middleware"
 
 module FlexDeploymentClient
   class UploadedFile < ModelBase
-    def file=(value)
-      binding.pry
-
-      if /^file:\/\//.match?(value)
-        filename = value.match(/^file:\/\/(.*)$/)[1]
-        mime_type = MIME::Types.type_for(filename).first.content_type
-        bin_data = File.open(filename, "rb") { |f| f.read }
-        super("data:#{mime_type};base64,#{Base64.encode64(bin_data)}")
-      elsif /^http(s?):\/\//.match?(value)
-        self.file_url = value
-      else
-        super
-      end
-    end
-
     def self.upload(attributes)
-      binding.pry
+      body = {data: {type: :uploaded_files, attributes: attributes}}
+      headers = {"Content-Type": "application/vnd.api+json"}
 
-      response = connection.run(:post, table_name, body: {data: {type: :uploaded_files, attributes: attributes}}, headers: custom_headers.merge({"Content-Type": "application/vnd.api+json"}))
+      response = connection.run(:post, table_name, body: body, headers: custom_headers.merge(headers))
+
       parser.parse(self, response).first
     end
   end
